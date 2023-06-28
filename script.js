9352597da597393f399726e4fa7f189fd65a0517
Gary H <3rdeye.ghj@gmail.com>
2023-06-28 11:44:36 -0700

    Basic message components built

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Just testing!")
+let messageComponents = {
+  greetings: ['Hello', 'Hi', 'Hey'],
+  names: ['John', 'Jane', 'Doe'],
+  messages: ['How are you?', 'Nice to meet you.', 'Good morning!']
+};
+
+let greeting = messageComponents.greetings[0]; // 'Hello'
+let person = messageComponents.names[1]; // 'Jane'
+let message = messageComponents.messages[2]; // 'Good morning!'
+
+let finalMessage = `${greeting}, ${person}. ${message}`;
+console.log(finalMessage); // 'Hello, Jane. Good morning!'
